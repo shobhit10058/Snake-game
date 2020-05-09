87f53f12b51c2d83f4be8b2b3ef66ab5087c2534
shobhit10058 <hitshob469@gmail.com>
2020-05-09 06:57:40 +0530

    commit

--- snake.js
+++ snake.js
@@ -1,9 +1,4 @@
-const cvs = document.getElementById("snake");
-const ctx = cvs.getContext("2d");
-
-
-function draw()
-{
-    ctx.fillRect(12,12,2,3);
-    
-}
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(20, 20, 150, 100);
